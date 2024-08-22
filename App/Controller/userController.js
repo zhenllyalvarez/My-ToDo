f1472b41482552587945b776d5331090ab822025
zhenllyalvarez <dev.zhenllyalvarez@gmail.com>
2024-08-22 11:24:34 +0800

    fix the db and also server.js

--- App/Controller/userController.js
+++ App/Controller/userController.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const path = require("path");
 
 router.get('/', (req, res) => {
-    res.sendfile(path.join(__dirname, "index.html"))
+    res.sendFile(path.join(__dirname, "../../index.html"));
 });
 
 module.exports = router;
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, "index.html"));
-// });
